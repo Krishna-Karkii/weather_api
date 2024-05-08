d0d623541230dfc5811abafb12d1862890df198c
krishna <krishnakarki1996@gmail.com>
2024-05-08 16:47:13 +0545

    read df and return temperature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,7 +11,12 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def data(station, date):
-    temperature = "12 degrees"
+    filepath = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    print(filepath)
+
+    # read df
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
     return {
         "station": station,
         "date": date,
